c079f7ec8785297618904a97dae7baa4df036395
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-30 14:52:23 -0300

    Implementação do Ico no topo da página e correção das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+   { 
+    path: '', 
+    redirectTo: 'pages/catalog', 
+    pathMatch: 'full' 
+  },
   {
     path: 'pages',
     loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
