9dfa34722bb1f525f23f5e7aa15efd5b71976c3b
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-02 13:15:00 -0300

    introdução de novos produtos

---
+++ src/app/catalog/catalog-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatalogServiceService {
+
+  public constructor() {}
+
+  public pesquisarBolo(nomeBusca: String){
+    
+  }
+}
--- src/app/catalog/catalog/catalog.component.ts
+++ src/app/catalog/catalog/catalog.component.ts
@@ -10,6 +10,18 @@ export class CatalogComponent {
   
   produtos = [
     {
+      nome: "Bolo de maracujá com cobertura",
+      descricao: "Bolo de maracujá com cobertura deliciosa de maracuja",
+      preco: 30.00,
+      imagem: "/imgs/bolo-maracuja-cobertura.jpg"
+    },
+    {
+      nome: "Bolo de maracujá simples",
+      descricao: "Bolo de maracujá simples, fofinho é delicioso",
+      preco: 25.00,
+      imagem: "/imgs/bolo-maracuja.jpg"
+    },
+    {
       nome: "Bolo de Banana com canela",
       descricao: "Bolo fofinho de banana com um delicado toque de canela.",
       preco: 20.00,
